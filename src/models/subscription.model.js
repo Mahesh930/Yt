b1052b1d4ad176124989dfe1c44be04266578fe8
Mahesh Vishnu Warpe <maheshvarpe930@gmail.com>
2024-11-05 15:13:10 +0530

    add subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from "mongoose";
+
+const subscriptionSchema = new Schema(
+    {
+        subscriber: {
+            type: Schema.Types.ObjectId, //one who is subscribing
+            ref: 'User',
+        },
+        channel: {
+            type: Schema.Types.ObjectId, //channel being subscribed to
+            ref: "User"
+        }
+    },
+    {
+        timestamps: true
+    });
+
+
+const Subscription = mongoose.model('Subscription', subscriptionSchema);
+
